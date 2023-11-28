4a46bf974e8b4968f3dc5a6bf1f477152903c9ab
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-28 16:37:57 +0100

    added wrapper on js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//1) Creiamo il nostro array di oggetti che rappresentano ciascun post.
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +57,54 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+//2)Prendendo come riferimento il layout di esempio presente nell'html,
+// stampiamo i post del nostro feed.
+const wrapperContainer = document.getElementById('container');
+
+posts.forEach((post)=>{
+    
+let counterLikes=0
+
+    wrapperContainer.innerHTML +=`
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">Phil Mangione</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+    })
+
+//3)Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. 
+//Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+//FUNCTION//
+function addCounterLikes(){
+    counterLikes++
+    
+}
